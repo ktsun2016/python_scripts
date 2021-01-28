f1529a03cf630eacf473e242a48b4abff7f17022
ktsun2016 <kuotungsun@gmail.com>
2021-01-28 15:10:16 -0800

    update check_pair.py

--- check_pair.py
+++ check_pair.py
@@ -1,59 +1,31 @@
 def check_pair(s):
-    '''
-    task1: check the total number of element in this string is
-           even, otherwise it should fail.
-    '''
-    if len(s)%2 !=0:
-        return False
-    else:
-        pass
-    
-    '''
-    task2: locate the index of the two symbols of each pair and check
-           1. same occurance
-           2. the index of leading symbol is less than its pair symbol
-    '''
     
     s1=list(s)
-    # task 2.1 for "(" and ")"
-    if "(" in s1 and ")" in s1:
-        l1=[i for i, x in enumerate(s1) if x == "("]
-        l2=[i for i, x in enumerate(s1) if x == ")"]
-        if len(l1)!=len(l2):
+    check=['(',')', '[',']', '{','}']
+    check_list=[['(',')'], ['[',']'], ['{','}']]
+    for i in range(len(s1)):
+        if s1[i] in check:
+            pass
+        else:
+            return False
+#     return all(element==s1 for element in check)
+    for i in range(len(check_list)):
+        if bool(check_list[i][0] in s1) ^ bool(check_list[i][1] in s1):
             return False
         else:
-            for i in range(len(l1)):
-                if l1[i] > l2[i]:
-                    return False
-                else:
-                    pass
-                
-    # task 2.1 for "[" and "]"
-    if "[" in s1 and "]" in s1:
-        l3=[i for i, x in enumerate(s1) if x == "["]
-        l4=[i for i, x in enumerate(s1) if x == "]"]
-        if len(l3)!=len(l4):
-            return False
-        else:
-            for i in range(len(l3)):
-                if l3[i] > l4[i]:
-                    return False
-                else:
-                    pass
-    
-    # task 2.1 for "{" and "}"
-    if "{" in s1 and "}" in s1:
-        l5=[i for i, x in enumerate(s1) if x == "{"]
-        l6=[i for i, x in enumerate(s1) if x == "}"]
-        if len(l5)!=len(l6):
-            return False
-        else:
-            for i in range(len(l5)):
-                if l5[i] > l6[i]:
-                    return False
-                else:
-                    pass
+            pass
+        if check_list[i][0] in s1 and check_list[i][1] in s1:
+            l1=[j for j, x in enumerate(s1) if x==check_list[i][0]]
+            l2=[j for j, x in enumerate(s1) if x==check_list[i][1]]
+            if len(l1) != len(l2):
+                return False
+            else:
+                for i in range(len(l1)):
+                    if l1[i]>l2[i]:
+                        return False
+                    else:
+                        pass
+        
     return True
-
-s='{}()[]'
+s='{}()[]00'
 print(check_pair(s))
